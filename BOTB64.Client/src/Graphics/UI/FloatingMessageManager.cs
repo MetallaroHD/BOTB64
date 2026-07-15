75d59ab58487ef9d49c985673fdd2ecc57e41697
MetallaroHD <cafolol@gmail.com>
2026-07-15 23:10:32 +0200

    text

---
+++ BOTB64.Client/src/Graphics/UI/FloatingMessageManager.cs
@@ -0,0 +1,21 @@
+﻿using RL = Raylib_cs;
+
+namespace BOTB64.Graphics.UI
+{
+    public static class FloatingMessageManager
+    {
+        static Label Message1;
+        static Label Message2;
+        static Label Message3;
+
+        static UIScreen Screen;
+        public static void Init(UIScreen screen)
+        {
+            Screen = screen;
+        }
+
+        public static void AddMessage(string message, int duration = 5, int fontSize = 10, RL.Color? color = null) 
+        {
+        }
+    }
+}
